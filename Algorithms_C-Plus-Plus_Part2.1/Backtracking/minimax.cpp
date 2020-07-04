0d9a888693952b93f883a4a12ce8465efce8f997
alvarohenriquecz <alvarohenriquecz@gmail.com>
2020-07-04 00:28:24 -0300

    2.1 Backtracking

---
+++ Algorithms_C-Plus-Plus_Part2.1/Backtracking/minimax.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::max;
+using std::min;
+using std::vector;
+
+int minimax(int depth, int node_index, bool is_max, vector<int> scores,
+        int heigth)
+        {
+            if (depth == heigth)
+                return scores[node_index];
+        int v1 = minimax(depth + 1, node_index * 2, !is_max, scores, heigth);
+        int v2 = minimax(depth + 1, node_index * 2 + 1, !is_max, scores, heigth);
+
+        return is_max ? max(v1, v2) : min(v1, v2);        
+}
+
+int main()
+{
+    vector<int> scores = {90, 23, 6, 33, 21, 65, 123, 34423 };
+    int heigth = log2(scores.size());
+
+    cout << "Optimal value: " << minimax(0, 0, true, scores, heigth) << endl;
+}
